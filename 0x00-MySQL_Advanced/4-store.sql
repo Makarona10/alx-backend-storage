7c61f9e1b4110546de7664062a3c3fe339a996ec
Makarona10 <ahmedneyssi@gmail.com>
2024-08-08 23:12:55 +0000

    Task 4 done

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,23 +1,8 @@
 --SQL script that creates a trigger that decreases
 --the quantity of an item after adding a new order.
 
-
---DELIMITER $$
---CREATE TRIGGER decrease_items
---AFTER INSERT ON orders
---FOR EACH ROW
---BEGIN
-  --  DECLARE qnt INT;
-
-    --SELECT NEW.number INTO qnt;
-
-    --UPDATE items SET quantity = quantity - qnt 
-   -- WHERE name = NEW.item_name;
-
---END$$
---DELIMITER ;
-
 CREATE TRIGGER buy_trigger
 AFTER INSERT ON orders
 FOR EACH ROW
 UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
+
