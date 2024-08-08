c375bc117f807958887bed5d00317684fd03cdde
Makarona10 <ahmedneyssi@gmail.com>
2024-08-08 22:42:58 +0300

    Task 2 done

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,6 @@
 --script that ranks country origins of bands
 --ordered by the number of (non-unique) fans
 
-mysqldump holberton > metal_bands.sql
-
 SELECT DISTINCT origin,
 (SELECT SUM(fans) FROM metal_bands m WHERE metal_bands.origin = m.origin)
 AS nb_fans FROM metal_bands ORDER BY nb_fans DESC;
